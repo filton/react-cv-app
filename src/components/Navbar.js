5ec6a2bc1b95fa06e8fd37408062d7d61c8f2347
Toni Filipovic <tonifilipovic18@Tonis-Air.lan>
2020-10-12 19:42:37 +0200

    removed unused Redirect import

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, NavLink, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
 import Profile from "./Profile";
 import SkillsAndEducation from "./SkillsAndEducation";
 import Projects from "./Projects";
